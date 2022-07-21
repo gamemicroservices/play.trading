07c1340e100c4d4f091766986817f99b0ad9149d
andretheprogrammer <andre.lundstrom@gmail.com>
2022-07-21 22:44:52 +0200

    First commit

---
+++ Play.Identity/src/Play.Identity.Service/Settings/IdentitySettings.cs
@@ -0,0 +1,11 @@
+namespace Play.Identity.Service.Settings
+{
+    public class IdentitySettings
+    {
+        public string AdminUserEmail { get; init; }
+
+        public string AdminUserPassword { get; init; }
+
+        public decimal StartingGil { get; init; }
+    }
+}
---
+++ Play.Trading/src/Play.Trading.Service/Settings/QueueSettings.cs
@@ -0,0 +1,9 @@
+namespace Play.Trading.Service.Settings
+{
+    public class QueueSettings
+    {
+        public string GrantItemsQueueAddress { get; init;}
+        public string DebitGilQueueAdress { get; init; }
+        public string SubtractItemsQueueAddress { get; init; }
+    }
+}
